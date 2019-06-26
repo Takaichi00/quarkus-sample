9e37492e98518c94ee10ba9ac5240b6f2a6a0d6f
Tomoaki Ubuntu <tomoaki.takaichi01@g.softbank.co.jp>
2019-06-26 11:32:22 +0900

    rest clientによる接続を確認

--- src/main/java/org/acme/quickstart/domain/client/BookRestClient.java
+++ src/main/java/org/acme/quickstart/domain/client/BookRestClient.java
@@ -1,9 +1,18 @@
 package org.acme.quickstart.domain.client;
 
-import org.acme.quickstart.domain.model.Book;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+import org.acme.quickstart.domain.model.Book;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/book")
+@RegisterRestClient
 public interface BookRestClient {
 
-  public Book getAllBooks();
+  @GET
+  @Path("/test")
+  public String getAllBooks();
   
 }
--- src/main/java/org/acme/quickstart/domain/service/BookServiceImpl.java
+++ src/main/java/org/acme/quickstart/domain/service/BookServiceImpl.java
@@ -8,15 +8,19 @@ import javax.inject.Inject;
 
 import org.acme.quickstart.domain.client.BookRestClient;
 import org.acme.quickstart.domain.model.Book;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class BookServiceImpl implements BookService {
 
   @Inject
+  @RestClient
   BookRestClient bookRestClient;
   
   @Override
   public List<Book> getAllBooks() {
+    String book = bookRestClient.getAllBooks();
+    System.out.println(book);
     return new ArrayList<Book>();
   }
 
