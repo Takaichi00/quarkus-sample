77547ce570361e9ef2c0e26fac3272397812e04c
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-06-06 16:57:47 +0900

    feat: return list json

--- src/main/java/com/takaichi00/sample/quarkus/application/controller/BookController.java
+++ src/main/java/com/takaichi00/sample/quarkus/application/controller/BookController.java
@@ -6,13 +6,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/v1/books")
 public class BookController {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public BookPayload getAllBooks() {
-    return BookPayload.builder().isbn("test-isbn").build();
+  public List<BookPayload> getAllBooks() {
+
+    return Arrays.asList(
+            BookPayload.builder().isbn("test-isbn").build()
+    );
   }
 }
--- src/test/java/com/takaichi00/sample/quarkus/application/BookControllerTest.java
+++ src/test/java/com/takaichi00/sample/quarkus/application/BookControllerTest.java
@@ -1,10 +1,15 @@
 package com.takaichi00.sample.quarkus.application;
 
+import com.takaichi00.sample.quarkus.application.payload.BookPayload;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
@@ -12,11 +17,14 @@ class BookControllerTest {
 
   @Test
   void test_v1books() {
-    given()
+    List<BookPayload> actual = given()
       .when()
         .get("/v1/books")
       .then()
         .statusCode(200)
-        .body("isbn", equalTo("test-isbn"));
+        .extract()
+        .body().jsonPath().getList(".", BookPayload.class);
+
+    assertThat(actual.get(0).getIsbn(), is("test-isbn"));
   }
 }
