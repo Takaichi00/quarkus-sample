0dbc5b36ce64a8a1e3be168ef254a40e2b6f257b
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-06-22 23:22:40 +0900

    feat: impl repository

--- src/main/java/com/takaichi00/sample/quarkus/integration/repository/BookRepositoryImpl.java
+++ src/main/java/com/takaichi00/sample/quarkus/integration/repository/BookRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.takaichi00.sample.quarkus.domain.model.Book;
 import com.takaichi00.sample.quarkus.domain.repository.BookRepository;
 import com.takaichi00.sample.quarkus.integration.entity.BookEntity;
 import io.quarkus.arc.DefaultBean;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
@@ -26,13 +28,16 @@ public class BookRepositoryImpl implements BookRepository {
     TypedQuery<BookEntity> query = entityManager.createQuery("From BookEntity", BookEntity.class);
     List<BookEntity> bookEntities = query.getResultList();
 
-    return Arrays.asList(
-            Book.builder()
-                    .isbn(bookEntities.get(0).getIsbn())
-                    .title("test-title")
-                    .authors(Arrays.asList("authors1", "authors2"))
-                    .price(1000)
-                    .build()
-    );
+    List<Book> books = new ArrayList<>();
+    for (BookEntity bookEntity : bookEntities) {
+      books.add(Book.builder()
+              .isbn(bookEntity.getIsbn())
+              .title("test-title")
+              .authors(Arrays.asList("authors1", "authors2"))
+              .price(1000)
+              .build());
+    }
+
+    return books;
   }
 }
