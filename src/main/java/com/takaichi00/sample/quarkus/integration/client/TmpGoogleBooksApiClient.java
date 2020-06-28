3e1f2ab1b44108168bf09088162f78de6550d699
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-06-28 15:25:00 +0900

    feat: tmp google api client

---
+++ src/main/java/com/takaichi00/sample/quarkus/integration/client/TmpGoogleBooksApiClient.java
@@ -0,0 +1,23 @@
+package com.takaichi00.sample.quarkus.integration.client;
+
+import com.takaichi00.sample.quarkus.domain.GoogleBooksApiClient;
+import com.takaichi00.sample.quarkus.domain.model.Book;
+import com.takaichi00.sample.quarkus.domain.model.Isbn;
+import java.util.Arrays;
+import java.util.List;
+
+public class TmpGoogleBooksApiClient implements GoogleBooksApiClient {
+
+  @Override
+  public List<Book> getAllBooks(List<Isbn> isbnList) {
+    return Arrays.asList(
+            Book.builder()
+                    .isbn("1234567890123")
+                    .title("test-title")
+                    .authors(Arrays.asList("authors1", "authors2"))
+                    .price(1000)
+                    .build()
+    );
+  }
+
+}
