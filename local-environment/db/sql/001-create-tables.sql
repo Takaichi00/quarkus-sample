021ac619ef8c0c4843f0852cc0099425025095e7
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-07-21 22:08:50 +0900

    chore: change init table sql

--- local-environment/db/sql/001-create-tables.sql
+++ local-environment/db/sql/001-create-tables.sql
@@ -1,12 +1,12 @@
 ---- drop ----
-DROP TABLE IF EXISTS `test_table`;
+DROP TABLE IF EXISTS `books`;
 
 ---- create ----
-create table IF not exists `test_table`
-(
- `id`               INT(20) AUTO_INCREMENT,
- `name`             VARCHAR(20) NOT NULL,
- `created_at`       Datetime DEFAULT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE books (
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    isbn VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+---- insert sample data ----
+INSERT INTO books(id, isbn) VALUES (1, '9784043636037');
