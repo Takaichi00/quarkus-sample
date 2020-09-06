b8f051ada6a680f9ad679508998ece5fc23736f7
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-09-06 15:49:09 +0900

    feat: using microprofile client

--- src/main/java/com/takaichi00/sample/quarkus/domain/client/GoogleBooksApiClientByMicroProfile.java
+++ src/main/java/com/takaichi00/sample/quarkus/domain/client/GoogleBooksApiClientByMicroProfile.java
@@ -5,13 +5,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/v1")
 @RegisterRestClient(configKey = "google-api")
 public interface GoogleBooksApiClientByMicroProfile {
 
   @GET
-  @Path("//books/v1/volumes/{q}")
+  @Path("/books/v1/volumes/{q}")
   @Produces("application/json")
-  GoogleReadApiResponse getByIsbn();
+  GoogleReadApiResponse getByIsbn(@PathParam String q);
 }
--- src/main/java/com/takaichi00/sample/quarkus/domain/service/BookServiceImpl.java
+++ src/main/java/com/takaichi00/sample/quarkus/domain/service/BookServiceImpl.java
@@ -2,14 +2,15 @@ package com.takaichi00.sample.quarkus.domain.service;
 
 import com.takaichi00.sample.quarkus.application.domain.BookService;
 import com.takaichi00.sample.quarkus.domain.client.GoogleBooksApiClient;
+import com.takaichi00.sample.quarkus.domain.client.GoogleBooksApiClientByMicroProfile;
 import com.takaichi00.sample.quarkus.domain.model.Book;
 import com.takaichi00.sample.quarkus.domain.model.Isbn;
 import com.takaichi00.sample.quarkus.domain.repository.BookmarkRepository;
-
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 // refer to: https://quarkus.io/blog/quarkus-dependency-injection/
 @ApplicationScoped
@@ -19,6 +20,10 @@ public class BookServiceImpl implements BookService {
   private final BookmarkRepository bookmarkRepository;
 
   private final GoogleBooksApiClient googleBooksApiClient;
+
+  @RestClient
+  @Inject
+  GoogleBooksApiClientByMicroProfile googleBooksApiClientByMicroProfile;
 
   @Override
   public List<Book> getAllBookmarks() {
@@ -34,6 +39,7 @@ public class BookServiceImpl implements BookService {
 
   @Override
   public Book searchBook(Isbn isbn) {
+    googleBooksApiClientByMicroProfile.getByIsbn("isbn:" + isbn);
     return googleBooksApiClient.getBook(isbn);
   }
 
