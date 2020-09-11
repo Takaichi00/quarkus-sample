9e39d80a825784ec31770258152fbbe3963e5259
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-09-11 10:16:11 +0900

    feat: impl MP Client but not start

--- src/main/java/com/takaichi00/sample/quarkus/integration/client/GoogleBooksApiMicroProfileClientImpl.java
+++ src/main/java/com/takaichi00/sample/quarkus/integration/client/GoogleBooksApiMicroProfileClientImpl.java
@@ -3,12 +3,20 @@ package com.takaichi00.sample.quarkus.integration.client;
 import com.takaichi00.sample.quarkus.domain.client.GoogleBooksApiMicroProfileClient;
 import com.takaichi00.sample.quarkus.domain.model.Book;
 import com.takaichi00.sample.quarkus.domain.model.Isbn;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class GoogleBooksApiMicroProfileClientImpl implements GoogleBooksApiMicroProfileClient {
+
+  @Inject
+  @RestClient
+  GoogleBooksApiClientByMicroProfile googleBooksApiClientByMicroProfile;
+
   @Override
   public Book getBook(Isbn isbn) {
+    googleBooksApiClientByMicroProfile.getByIsbn("isbn:" + isbn);
     return null;
   }
 }
