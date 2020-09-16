668dc54cdbbfbc955f29b762ea6cdd17a36b0517
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-09-16 18:25:29 +0900

    refactor: reindent, refactor package

--- rest-client-quickstart/src/main/java/org/acme/rest/client/CountriesService.java
+++ rest-client-quickstart/src/main/java/org/acme/rest/client/CountriesService.java
@@ -1,12 +1,11 @@
 package org.acme.rest.client;
 
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.Set;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/v2")
 @RegisterRestClient
