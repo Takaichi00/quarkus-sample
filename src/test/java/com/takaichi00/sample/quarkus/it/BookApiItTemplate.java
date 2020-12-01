98435b2b9f2cd436a355fc36f78feb1c5b06198c
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-12-01 20:23:52 +0900

    test: add test

--- src/test/java/com/takaichi00/sample/quarkus/it/BookApiItTemplate.java
+++ src/test/java/com/takaichi00/sample/quarkus/it/BookApiItTemplate.java
@@ -11,7 +11,7 @@ public class BookApiItTemplate extends IntegrationTestTemplate {
 
 
   @Test
-  void test_v1BooksApi() {
+  void test_v1GetBooksApi() {
     // setup
     BookPayload expected = BookPayload.builder().isbn("9784043636037")
                                                 .title("アラビアの夜の種族")
@@ -23,6 +23,28 @@ public class BookApiItTemplate extends IntegrationTestTemplate {
     BookPayload actual = given().when()
                                   .pathParam("isbn", "9784043636037")
                                   .get("/v1/books/{isbn}")
+                                .then()
+                                  .statusCode(200)
+                                .extract()
+                                  .body().jsonPath().getObject(".", BookPayload.class);
+
+    // assert
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void test_v2GetBooksApi() {
+    // setup
+    BookPayload expected = BookPayload.builder().isbn("9784043636037")
+                                                .title("アラビアの夜の種族")
+                                                .authors(Arrays.asList("古川日出男"))
+                                                .url("http://books.google.co.jp/books?id=s4CRHAAACAAJ&dq=isbn:9784043636037&hl=&cd=1&source=gbs_api")
+                                                .build();
+
+    // execute
+    BookPayload actual = given().when()
+                                  .pathParam("isbn", "9784043636037")
+                                  .get("/v2/books/{isbn}")
                                 .then()
                                   .statusCode(200)
                                 .extract()
