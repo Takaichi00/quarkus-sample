da3cfa7a037c3f82740ffa8ee99dd4c19c73d0e3
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-07-05 20:16:11 +0900

    chore: experiment heavisideUsers

--- src/test/scala/computerdatabase/QuarkusSampleSimulation.scala
+++ src/test/scala/computerdatabase/QuarkusSampleSimulation.scala
@@ -21,7 +21,8 @@ class QuarkusSampleSimulation extends Simulation {
     // https://gatling.io/docs/gatling/reference/current/general/simulation_setup/
 //        atOnceUsers(1), // 1回で x スレッド立ち上げてリクエストを実施
 //        rampUsers(10).during(1.seconds), // x 秒かけて y スレッド立ち上げてリクエストを実施
-        constantUsersPerSec(5).during(2) // 1秒ごとに x スレッド、y 秒間実施
+//        constantUsersPerSec(5).during(2), // 1秒ごとに x スレッド、y 秒間実施
+        heavisideUsers(100).during(10.seconds) // https://en.wikipedia.org/wiki/Heaviside_step_function#/media/File:Step_function_approximation.png のグラフのように、中間の時間まで徐々に単位時間あたりのリクエストが増加し、徐々に単位時間あたりのリクエストが減少する
       )
     ).assertions(
       global.responseTime.max.lt(200), // https://gatling.io/docs/gatling/reference/current/general/assertions/
