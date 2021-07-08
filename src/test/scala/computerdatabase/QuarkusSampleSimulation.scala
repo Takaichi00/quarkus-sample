7ffa197e68216872de69896f7b8e9893fa8d745f
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-07-08 21:50:37 +0900

    chore: experiment pause and child scenario

--- src/test/scala/computerdatabase/QuarkusSampleSimulation.scala
+++ src/test/scala/computerdatabase/QuarkusSampleSimulation.scala
@@ -14,27 +14,32 @@ class QuarkusSampleSimulation extends Simulation {
   val sampleScenario = scenario("Sample") // A scenario is a chain of requests and pauses
     .exec(http("request_hello")
       .get("/v1/hello"))
-    .pause(1) // Note that Gatling has recorder real time pauses
+    .pause(5) // 実行後に x 秒 pause する
 
-  val livenessScenario = scenario("Liveness") // A scenario is a chain of requests and pauses
+  val livenessScenario = scenario("Liveness")
     .exec(http("request_hello")
       .get("/q/health/live"))
-    .pause(1) // Note that Gatling has recorder real time pauses
 
+  val readinessScenario = scenario("readiness")
+    .exec(http("request_hello")
+      .get("/q/health/ready"))
 
   // https://hkawabata.github.io/technical-note/note/OSS/gatling.html
   setUp(sampleScenario.inject(
     // https://gatling.io/docs/gatling/reference/current/general/simulation_setup/
-        atOnceUsers(1), // 1回で x スレッド立ち上げてリクエストを実施
+//        atOnceUsers(1), // 1回で x スレッド立ち上げてリクエストを実施
 //        rampUsers(10).during(1.seconds), // x 秒かけて y スレッド立ち上げてリクエストを実施
 //        heavisideUsers(100).during(10.seconds) // https://en.wikipedia.org/wiki/Heaviside_step_function#/media/File:Step_function_approximation.png のグラフのように、中間の時間まで徐々に単位時間あたりのリクエストが増加し、徐々に単位時間あたりのリクエストが減少する
-//        constantUsersPerSec(2).during(10), // 1秒ごとに x スレッド、y 秒間実施 (open model)
+        constantUsersPerSec(2).during(10), // 1秒ごとに x スレッド、y 秒間実施 (open model)
 //        constantConcurrentUsers(2).during(10.seconds) // 1秒ごとに x スレッド、y 秒間実施 (closed model)
     // Open model と Closed model の違い: https://gatling.io/2020/04/how-easily-can-i-perform-a-load-test/ , https://gatling.io/docs/gatling/reference/current/general/simulation_setup/#open-vs-closed-workload-models
-      ),
-    livenessScenario.inject(atOnceUsers(1))
+      ),livenessScenario.inject(atOnceUsers(1))
     ).assertions(
       global.responseTime.max.lt(200), // https://gatling.io/docs/gatling/reference/current/general/assertions/
       global.successfulRequests.percent.gt(95)
     ).protocols(httpProtocol)
+    .pauses(disabledPauses) // pause を設定しても無効化する
+
+  // Scaling Out (https://gatling.io/docs/gatling/guides/scaling_out/)
+  // ↑ 負荷が高い Gatling のテストを実施するには
 }
